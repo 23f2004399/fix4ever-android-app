f0015d0f635a4ddb54e2b4ccc89c63e6797a6900
Mrinal <harshkr918@gmail.com>
2026-08-02 01:08:21 +0530

    fix non scroll in login

--- src/features/auth/ResetPasswordScreen.tsx
+++ src/features/auth/ResetPasswordScreen.tsx
@@ -271,7 +271,7 @@ export function ResetPasswordScreen({
   return (
     <KeyboardAvoidingView
       style={styles.container}
-      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       keyboardVerticalOffset={0}
     >
       <ScrollView
